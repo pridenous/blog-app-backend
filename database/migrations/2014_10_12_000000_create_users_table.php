083e8599bb2b1626aad459e6c5faf8cc495451b5
pridenous <adliputrap@gmail.com>
2021-04-26 15:17:47 +0700

    Post Models

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,6 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->integer('number')->unsigned();
-            $table->string('password');
             $table->string('user_type')->default(User::REGULAR_USER);
             $table->rememberToken();
             $table->timestamps();
