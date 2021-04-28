d24d55424a455ff90a66909167e73141b3451b5b
pridenous <adliputrap@gmail.com>
2021-04-28 11:05:46 +0700

    Make new User -C -M

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,20 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    use HasFactory;
     protected $fillable = [
         'name',
         'email',
@@ -27,28 +19,8 @@ class User extends Authenticatable
     const ADMIN_USER = 'Admin';
     const REGULAR_USER = 'Editor';
 
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-
-    public function posts()
-    {
-        return $this->hasMany(Posts::class);
-    }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::resource('/users', UserController::class);
+
+// Route::put('/users/{id}', [UserController::class, 'update']);
