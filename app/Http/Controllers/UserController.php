aac1b185503464e650c3e69cf5eef7e9ba40cc9e
pridenous <adliputrap@gmail.com>
2021-04-28 11:31:05 +0700

    Make Store and Delete Method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
@@ -21,10 +22,47 @@ class UserController extends Controller
         return ['data' => $user];
     }
 
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => $validator->errors(),
+            ], 403);
+        }
+
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->number = $request->number;
+        $user->user_type = $request->user_type;
+
+        $user->save();
+        return response()->json([
+            "status" => true,
+            "message" => "Data User berhasil disimpan..",
+            "data" => $user
+        ]);
+    }
+
     public function update($id, Request $request)
     {
         $user = User::findOrFail($id);
         $user->update($request->all());
         return response()->json($user);
+    }
+
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+        return ['users' => $user];
     }
 }
